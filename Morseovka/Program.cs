060b3dd41102c0783906aaab48797f60d8a76481
Martin Roštejnský <martin.rostejnsky@email.cz>
2023-01-11 11:20:04 +0100

    slovniky

--- Morseovka/Program.cs
+++ Morseovka/Program.cs
@@ -5,4 +5,4 @@ Console.WriteLine("Hello, World!");
 
 MorseEncoder morseEncoder = new MorseEncoder();
 
-Console.WriteLine(morseEncoder.Encode("ano"));
+Console.WriteLine(morseEncoder.Encode("achjo"));
