f2cc8f3f45a18f16138f37fa0d1d13976c898517
Martin Roštejnský <martin.rostejnsky@email.cz>
2023-01-11 10:36:51 +0100

    basic encoding

---
+++ Morseovka/MorseEncoder.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Morseovka
+{
+    class MorseEncoder
+    {
+
+        Dictionary<char, string> Morse = new Dictionary<char, string>()
+        {
+            { 'a', ".-" },
+            { 'b', "-..." },
+            { 'c', "-.-." },
+            { 'd', "-.." },
+            { 'e', "." },
+            { 'f', "..-." },
+            { 'g', "--." },
+            { 'h', "...." },
+            { 'i', ".." },
+            { 'j', ".---" },
+            { 'k', "-.-" },
+            { 'l', ".-.." },
+            { 'm', "--" },
+            { 'n', "-." },
+            { 'o', "---" },
+            { 'p', ".--." },
+            { 'q', "--.-" },
+            { 'r', ".-." },
+            { 's', "..." },
+            { 't', "-" },
+            { 'u', "..-" },
+            { 'v', "...-" },
+            { 'w', ".--" },
+            { 'x', "-..-" },
+            { 'y', "-.--" },
+            { 'z', "--.." },
+            { '0', "-----" },
+            { '1', ".----" },
+            { '2', "..---" },
+            { '3', "...--" },
+            { '4', "....-" },
+            { '5', "....." },
+            { '6', "-...." },
+            { '7', "--..." },
+            { '8', "---.." },
+            { '9', "----." },
+            { ' ', " " },
+            { '.', ".-.-.-" },
+            { ',', "--..--" },
+            { '?', "..--.." },
+            { '!', "-.-.--" },
+            { '(', "-.--." },
+            { ')', "-.--.-" },
+            { '&', ".-..." },
+            { ':', "---..." },
+            { ';', "-.-.-." },
+            { '=', "-...-" },
+            { '+', ".-.-." },
+            { '-', "-....-" },
+            { '_', "..--.-" },
+            { '"', ".-..-." },
+            { '$', "...-..-" },
+            { '@', ".--.-." },
+            /*{ 'ch', "-.-." }*/
+        };
+        public string Encode(string text)
+        {
+
+            List<char> chars= new List<char>();
+            chars.Add('@');
+            chars.Add('@');
+            List<string> coded = new List<string>();
+
+            foreach (char c in chars) {
+                coded.Add(Morse[c]);
+            }
+
+            string code = string.Join("/", coded);
+            return code;
+        }
+
+        public string Decode(string code)
+        {
+            string[] mlsoun = { "a", "b", "c" };
+            string text = string.Join("", mlsoun);
+
+            return text;
+        }
+
+    }
+}
--- Morseovka/Program.cs
+++ Morseovka/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Morseovka;
+
 Console.WriteLine("Hello, World!");
+
+MorseEncoder morseEncoder = new MorseEncoder();
+
+Console.WriteLine(morseEncoder.Encode("ano"));
